065248ef1291d495aff0037812a64456b7ea4d2d
RaynorLEE <muzhili1997@gmail.com>
2021-03-03 04:57:25 +0800

    AlexNet

--- model.py
+++ model.py
@@ -13,41 +13,8 @@ class AlexNet(nn.Module):
         super(AlexNet, self).__init__()
         # TODO: implement alex net for task 1; 
         # You are free to change channels, kernel sizes, strides, etc. But the model's flops must be smaller than 200M.
-        # self.conv1 = nn.Sequential(
-        #     nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, padding=2),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(2, stride=2)
-        # )
-        # self.conv2 = nn.Sequential(
-        #     nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, padding=2),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(2, stride=2)
-        # )
-        # self.conv3 = nn.Sequential(
-        #     nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-        #     nn.ReLU()
-        # )
-        # self.conv4 = nn.Sequential(
-        #     nn.Conv2d(in_channels=64, out_channels=96, kernel_size=3, padding=1),
-        #     nn.ReLU()
-        # )
-        # self.conv5 = nn.Sequential(
-        #     nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3, padding=1),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(2, stride=2)
-        # )
-        # self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        # self.fc1 = nn.Sequential(
-        #     nn.Linear(128, 128),
-        #     nn.ReLU()
-        # )
-        # self.fc2 = nn.Sequential(
-        #     nn.Linear(128, 64),
-        #     nn.ReLU()
-        # )
-        # self.fc3 = nn.Linear(64, 10)
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=5, padding=2),
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
