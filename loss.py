1b5cd1a4e37b736807e20566dae229680e3adae2
RaynorLEE <muzhili1997@gmail.com>
2021-03-05 02:55:11 +0800

    AlexNet loss func

--- loss.py
+++ loss.py
@@ -11,16 +11,16 @@ class CrossEntropyLoss(nn.Module):
         #   self.loss_func = nn.CrossEntropyLoss()
 
     def forward(self, x, y, epsilon=1e-12, **kwargs):
-        x = torch.clamp(x, 0, 1.0-epsilon)
-        exp_x = torch.exp(x)
-        mat = torch.zeros(exp_x.size(), device=exp_x.device)
-        for i in range(y.size()[0]):
-            mat[i][y[i]] = 1
-        loss = -torch.sum(torch.mul(mat, x), dim=-1) + torch.log(torch.sum(exp_x, dim=-1))
-        # x = torch.clamp(x, 0, 1.0 - epsilon)
-        # exp_sum = torch.sum(torch.exp(x), dim=-1)
-        # y = torch.unsqueeze(y, 1)
-        # x = torch.gather(dim=1, input=x, index=y)
-        # loss = -x + torch.log(exp_sum)
+        # x = torch.clamp(x, 0, 1.0-epsilon)
+        # exp_x = torch.exp(x)
+        # mat = torch.zeros(exp_x.size(), device=exp_x.device)
+        # for i in range(y.size()[0]):
+        #     mat[i][y[i]] = 1
+        # loss = -torch.sum(torch.mul(mat, x), dim=-1) + torch.log(torch.sum(exp_x, dim=-1))
+        x = torch.clamp(x, 0, 1.0 - epsilon)
+        log_sum_exp = torch.logsumexp(x, dim=1)
+        y = torch.unsqueeze(y, 1)
+        x = torch.gather(dim=1, input=x, index=y)
+        loss = -x + log_sum_exp
         return loss.mean()
         #   return self.loss_func(x, y)
