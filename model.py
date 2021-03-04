86d2a455737d17104105b828244870792232dd20
RaynorLEE <muzhili1997@gmail.com>
2021-03-05 05:23:37 +0800

    restrict 200m

--- model.py
+++ model.py
@@ -24,28 +24,28 @@ class AlexNet(nn.Module):
             nn.MaxPool2d(2, stride=2)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
             nn.ReLU()
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
             nn.ReLU()
         )
         self.conv5 = nn.Sequential(
-            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
-        self.avg_pool = nn.AdaptiveAvgPool2d((4, 4))
+        self.avg_pool = nn.AdaptiveAvgPool2d((2, 2))
         self.fc1 = nn.Sequential(
-            nn.Linear(4096, 4096),
+            nn.Linear(1024, 1024),
             nn.ReLU()
         )
         self.fc2 = nn.Sequential(
-            nn.Linear(4096, 1024),
+            nn.Linear(1024, 512),
             nn.ReLU()
         )
-        self.fc3 = nn.Linear(1024, 10)
+        self.fc3 = nn.Linear(512, 10)
         # self.conv1 = nn.Sequential(
         #     nn.Conv2d(in_channels=3, out_channels=96, kernel_size=7, stride=2, padding=2),
         #     nn.ReLU(),
@@ -87,7 +87,7 @@ class AlexNet(nn.Module):
         x = self.conv4(x)
         x = self.conv5(x)
         x = self.avg_pool(x)
-        x = x.view(-1, 4096)
+        x = x.view(-1, 1024)
         #   x = x.view(-1, 256*3*3)
         x = self.fc1(x)
         x = self.fc2(x)
