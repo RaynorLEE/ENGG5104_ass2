22061b617dec1bd056efadf1249d21efd914be9b
RaynorLEE <muzhili1997@gmail.com>
2021-03-05 03:11:36 +0800

    AlexNet loss func

--- loss.py
+++ loss.py
@@ -17,10 +17,17 @@ class CrossEntropyLoss(nn.Module):
         # for i in range(y.size()[0]):
         #     mat[i][y[i]] = 1
         # loss = -torch.sum(torch.mul(mat, x), dim=-1) + torch.log(torch.sum(exp_x, dim=-1))
-        x = torch.clamp(x, 0, 1.0 - epsilon)
-        log_sum_exp = torch.logsumexp(x, dim=1)
-        y = torch.unsqueeze(y, 1)
-        x = torch.gather(dim=1, input=x, index=y)
-        loss = -x + log_sum_exp
+        # x = torch.clamp(x, 0, 1.0 - epsilon)
+        # log_sum_exp = torch.logsumexp(x, dim=1)
+        # y = torch.unsqueeze(y, 1)
+        # x = torch.gather(dim=1, input=x, index=y)
+        # loss = -x + log_sum_exp
+        # return loss.mean()
+        loss = torch.zeros(y.shape, dtype=x.dtype, device=x.device)
+
+        for i, cls in enumerate(y):
+            x_class = -x[i][cls]
+            log_x_j = torch.log(torch.sum(torch.exp(x[i])))
+            loss[i] = x_class + log_x_j
         return loss.mean()
         #   return self.loss_func(x, y)
