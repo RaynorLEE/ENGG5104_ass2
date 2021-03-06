816736c835a249f24573e1905df143cc85e8c2cf
RaynorLEE <muzhili1997@gmail.com>
2021-03-07 04:55:27 +0800

    loss calculation

--- model.py
+++ model.py
@@ -13,38 +13,38 @@ class AlexNet(nn.Module):
         # TODO: implement alex net for task 1; 
         # You are free to change channels, kernel sizes, strides, etc. But the model's flops must be smaller than 200M.
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=128, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=96, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU()
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU()
         )
         self.conv5 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=128, out_channels=96, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
         self.avg_pool = nn.AdaptiveAvgPool2d((2, 2))
         self.fc1 = nn.Sequential(
-            nn.Linear(1024, 1024),
+            nn.Linear(384, 128),
             nn.ReLU()
         )
         self.fc2 = nn.Sequential(
-            nn.Linear(1024, 512),
+            nn.Linear(128, 128),
             nn.ReLU()
         )
-        self.fc3 = nn.Linear(512, 10)
+        self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv1(x)
@@ -53,7 +53,7 @@ class AlexNet(nn.Module):
         x = self.conv4(x)
         x = self.conv5(x)
         x = self.avg_pool(x)
-        x = x.view(-1, 1024)
+        x = x.view(-1, 384)
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.fc3(x)
