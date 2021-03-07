0b388f8b35fbf0924c103a1da07e1d4263483afb
RaynorLEE <muzhili1997@gmail.com>
2021-03-07 17:25:15 +0800

    reverse test task3

--- loss.py
+++ loss.py
@@ -33,6 +33,7 @@ class CrossEntropyLoss(nn.Module):
             print(']')
 
             self.weight = torch.as_tensor(data=weight, dtype=torch.float32, device=device)
+            self.ce = nn.CrossEntropyLoss(weight=self.weight)
 
 
     def forward(self, x, y, epsilon=1e-12, **kwargs):
@@ -40,18 +41,20 @@ class CrossEntropyLoss(nn.Module):
         # if self.task == 4:
         #     x = self.weight * x
         #     #   x = torch.einsum('c,bc->bc', self.weight, x)
-        log_sum_exp = torch.logsumexp(x, dim=1)
-        y = torch.unsqueeze(y, 1)
-        x = torch.gather(dim=1, input=x, index=y)
-        x = x.squeeze(-1)
-        loss = -x + log_sum_exp
-        if self.task == 4:
-            curr_weight = torch.zeros(loss.size(), dtype=x.dtype, device=x.device)
-            for i in range(len(y)):
-                curr_weight[i] = self.weight[y[i]]
-            loss = curr_weight * loss
-        #     result = loss.sum() / curr_weight.sum()
-        # else:
-        #     result = loss.mean()
-        # return result
-        return loss.mean()
+        # log_sum_exp = torch.logsumexp(x, dim=1)
+        # y = torch.unsqueeze(y, 1)
+        # x = torch.gather(dim=1, input=x, index=y)
+        # x = x.squeeze(-1)
+        # loss = -x + log_sum_exp
+        # if self.task == 4:
+        #     curr_weight = torch.zeros(loss.size(), dtype=x.dtype, device=x.device)
+        #     for i in range(len(y)):
+        #         curr_weight[i] = self.weight[y[i]]
+        #     loss = curr_weight * loss
+        # #     result = loss.sum() / curr_weight.sum()
+        # # else:
+        # #     result = loss.mean()
+        # # return result
+        # return loss.mean()
+        loss = self.ce(x, y)
+        return loss
