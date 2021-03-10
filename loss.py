5dda1f108d4fb2c6a3e826d32d3a6a3bfc782362
RaynorLEE <muzhili1997@gmail.com>
2021-03-11 00:31:56 +0800

    Q4

--- loss.py
+++ loss.py
@@ -14,7 +14,7 @@ class CrossEntropyLoss(nn.Module):
             cls_count = kwargs['cls_count']
             #   N = kwargs['dataset_size']
             #   beta = float(N - 1) / N
-            beta = 0.9999
+            beta = 0.9995
             device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
             # self.weight = torch.ones(size=[len(cls_count)], dtype=torch.float32, device=device)
             weight = []
@@ -37,25 +37,23 @@ class CrossEntropyLoss(nn.Module):
 
     def forward(self, x, y, epsilon=1e-12, **kwargs):
         #   x = torch.einsum('c,bc->bc', self.weight, x)
-        if self.task == 4:
-            epo = kwargs['epo']
-            if epo >= 80:
-                x = self.weight * x
+        if self.task == 4 and kwargs['epo'] >= 80:
+            x = self.weight * x
             #   x = torch.einsum('c,bc->bc', self.weight, x)
         log_sum_exp = torch.logsumexp(x, dim=1)
         y = torch.unsqueeze(y, 1)
         x = torch.gather(dim=1, input=x, index=y)
         x = x.squeeze(-1)
         loss = -x + log_sum_exp
-        if self.task == 4:
+        if self.task == 4 and kwargs['epo'] >= 80:
             curr_weight = torch.zeros(loss.size(), dtype=x.dtype, device=x.device)
             for i in range(len(y)):
                 curr_weight[i] = self.weight[y[i]]
             loss = curr_weight * loss
-        #     result = loss.sum() / curr_weight.sum()
-        # else:
-        #     result = loss.mean()
-        # return result
-        return loss.mean()
+            result = loss.sum() / curr_weight.sum()
+        else:
+            result = loss.mean()
+        return result
+        #   eturn loss.mean()
         #   loss = self.ce(x, y)
         #   return loss
